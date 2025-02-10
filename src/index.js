080220003d2af1841fca9156757d0d8e39320db9
Muhammad Ahmad <ranaahmad131@gmail.com>
2025-02-10 13:42:46 +0500

    Successfully passed the quiz

--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import StarComp from './StarRating';
+// import StarComp from './StarRating';
+import App from './App-quiz';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarComp 
-    fill = {"yellow"}
-  message = {["Poor", "Okay", "Avg", "Good", "Excellent"]
-}
-
-    />
+    <App />
+    {/* <StarComp
+      fill={"yellow"}
+      message={["Poor", "Okay", "Avg", "Good", "Excellent"]
+      } /> */}
   </React.StrictMode>
 );
 
