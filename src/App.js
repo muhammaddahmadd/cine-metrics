c6c2aee8c807ce6d2b8e8534180072cd3f87f0ee
Muhammad Ahmad <133768693+muhammaddahmadd@users.noreply.github.com>
2025-06-16 23:15:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ import { useKey } from "./useKey";
 //       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
 //   },
 // ];
-
 // const tempWatchedData = [
 //   {
 //     imdbID: "tt1375666",
