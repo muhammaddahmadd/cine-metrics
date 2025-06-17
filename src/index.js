46fde1719b69882037b5ae2b8930333557cbba0e
Muhammad Ahmad <133768693+muhammaddahmadd@users.noreply.github.com>
2025-06-17 23:02:35 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 // import StarComp from './StarRating';
 // import App from './App-quiz';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
