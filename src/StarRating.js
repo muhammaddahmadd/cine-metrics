78540550d4a150df88c795012d1ef4219467cdec
Muhammad Ahmad <133768693+muhammaddahmadd@users.noreply.github.com>
2025-10-02 23:23:54 +0500

    Update StarRating.js

--- src/StarRating.js
+++ src/StarRating.js
@@ -36,7 +36,7 @@ export default function StarComp({ maxRating = 5, fill = "orange", message = [],
     onSetRating(x)
     // console.log(x, "inisde star comp")
     }
-    // console.log(hoveredRating)
+    
 
     return (
         <div style={containerStyle}>
